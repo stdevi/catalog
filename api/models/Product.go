a18d0741b2b0ca8a005de4258e6ee884aaf29ef8
stdevi <dvstas00@gmail.com>
2020-04-03 17:20:35 +0300

    Add product FindAllByCategoryId func Fix Save and Update func

--- api/models/Product.go
+++ api/models/Product.go
@@ -1,18 +1,24 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+)
 
 type Product struct {
-	ID          uint    `gorm:"primary_key;auto_increment"`
-	Name        string  `gorm:"not null"`
-	Description string  `gorm:"not null"`
-	Price       float32 `gorm:"not null"`
-	CategoryID  uint    `gorm:"foreignkey:category_id"`
-	Category    Category
+	ID          uint     `gorm:"primary_key;auto_increment" json:"id"`
+	Name        string   `gorm:"not null" json:"name"`
+	Description string   `gorm:"not null" json:"description"`
+	Price       float32  `gorm:"not null" json:"price"`
+	CategoryID  uint     `gorm:"foreignkey:category_id;not null" json:"category_id"`
+	Category    Category `json:"category"`
 }
 
 func (p *Product) Save(db *gorm.DB) (*Product, error) {
 	if err := db.Save(&p).Error; err != nil {
+		return nil, err
+	}
+
+	if err := db.Take(&p.Category, "id = ?", p.CategoryID).Error; err != nil {
 		return nil, err
 	}
 
@@ -24,12 +30,12 @@ func (p *Product) Update(db *gorm.DB, id uint) (*Product, error) {
 		"name":        p.Name,
 		"description": p.Description,
 		"price":       p.Price,
-		"category_id": p.Category.ID,
+		"category_id": p.CategoryID,
 	}).Error; err != nil {
 		return nil, err
 	}
 
-	if err := db.Where("id = ?", id).Take(&p).Error; err != nil {
+	if err := db.Take(&p.Category, "id = ?", p.CategoryID).Error; err != nil {
 		return nil, err
 	}
 
@@ -50,13 +56,34 @@ func (p *Product) FindAll(db *gorm.DB) ([]*Product, error) {
 		return nil, err
 	}
 
+	if len(ps) > 0 {
+		for _, p := range ps {
+			if err := db.Take(&p.Category, "id = ?", p.CategoryID).Error; err != nil {
+				return nil, err
+			}
+		}
+	}
+
 	return ps, nil
 }
 
-func (p *Product) FindAllByCategory(db *gorm.DB, category Category) ([]*Product, error) {
-	ps := make([]*Product, 0)
-	if err := db.Model(&category).Related(&ps).Error; err != nil {
+func (p *Product) FindAllByCategoryId(db *gorm.DB, id uint) ([]*Product, error) {
+	c, err := (&Category{}).FindById(db, id)
+	if err != nil {
 		return nil, err
+	}
+
+	ps := make([]*Product, 0)
+	if err := db.Model(&c).Related(&ps).Error; err != nil {
+		return nil, err
+	}
+
+	if len(ps) > 0 {
+		for _, p := range ps {
+			if err := db.Take(&p.Category, "id = ?", p.CategoryID).Error; err != nil {
+				return nil, err
+			}
+		}
 	}
 
 	return ps, nil
