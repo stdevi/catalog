0333ca75337049e32ea0746546fd5101010431ce
stdevi <dvstas00@gmail.com>
2020-04-03 15:22:26 +0300

    Add category controller UpdateCategory func

--- api/controllers/category_controller.go
+++ api/controllers/category_controller.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"catalog/api/models"
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 func (s *Server) CreateCategory(w http.ResponseWriter, r *http.Request) {
@@ -28,6 +30,38 @@ func (s *Server) CreateCategory(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Add("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(cCreated); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *Server) UpdateCategory(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.ParseUint(mux.Vars(r)["id"], 10, 32)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+
+	c := models.Category{}
+	if err := json.Unmarshal(b, &c); err != nil {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+
+	cUpdated, err := c.Update(s.DB, uint(id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(cUpdated); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -3,4 +3,5 @@ package controllers
 func (s *Server) initRoutes() {
 	s.Router.HandleFunc("/api/categories", s.GetCategories).Methods("GET")
 	s.Router.HandleFunc("/api/categories", s.CreateCategory).Methods("POST")
+	s.Router.HandleFunc("/api/categories/{id}", s.UpdateCategory).Methods("PUT")
 }
